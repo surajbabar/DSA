676da60cce5c298920bb2bf1d31b46671c1f2a14
suraj <surajbab@thoughtworks.com>
2013-12-28 19:51:40 +0530

    testing done , ready to use

--- bubble-sort/bubbleSortTest.c
+++ bubble-sort/bubbleSortTest.c
@@ -1,22 +1,17 @@
 #include "bubbleSort.h"
 #include "testUtils.h"
-#include <string.h>
-#include "../insertion-sort/sort.h"
 
 int compareInts(void* first,void* second){
     return *(int*)first - *(int*)second;
 };
-
 int compareChars(void* first,void* second){
     return *(char*)first - *(char*)second;
 };
-
 int compareFloats(void* first,void* second){
-        return *(float*)first - *(float*)second;
+    return *(float*)first - *(float*)second;
 };
-
 int compareDoubles(void* first,void* second){
-        return *(double*)first - *(double*)second;
+    return *(double*)first - *(double*)second;
 };
 
 void test_sort_integers_using_bubble_Sort(){
@@ -25,44 +20,43 @@ void test_sort_integers_using_bubble_Sort(){
     for(i = 0; i < noOfElements; i++)
         actual[i] = &elementsToSort[i];
     bSort(actual,noOfElements,compareInts);
-    sort(elementsToSort,noOfElements,sizeof(int),compareInts);
-        ASSERT(expected[0] == *(int*)actual[0]);
-        ASSERT(expected[1] == *(int*)actual[1]);
-        ASSERT(expected[2] == *(int*)actual[2]);
-}
+    ASSERT(expected[0] == *(int*)actual[0]);
+    ASSERT(expected[1] == *(int*)actual[1]);
+    ASSERT(expected[2] == *(int*)actual[2]);
+};
 
 void test_sort_chars_using_bSort(){
-        int i,noOfElements = 3;
-        char elementsToSort[] = {'b','c','a'}, expected[] = {'a','b','c'};
-        void *actual[3];
-        for(i = 0; i < noOfElements; i++)
-                actual[i] = &elementsToSort[i];
+    int i,noOfElements = 3;
+    char elementsToSort[] = {'b','c','a'}, expected[] = {'a','b','c'};
+    void *actual[3];
+    for(i = 0; i < noOfElements; i++)
+        actual[i] = &elementsToSort[i];
     bSort(actual,noOfElements,compareChars);
-           ASSERT(expected[0] == *(char*)actual[0]);
-           ASSERT(expected[1] == *(char*)actual[1]);
-           ASSERT(expected[2] == *(char*)actual[2]);
-}
+    ASSERT(expected[0] == *(char*)actual[0]);
+    ASSERT(expected[1] == *(char*)actual[1]);
+    ASSERT(expected[2] == *(char*)actual[2]);
+};
 
 void test_sort_floats_using_bSort(){
-        int i,noOfElements = 3;
-        float elementsToSort[] = {1.2, 3.2, 2.2}, expected[] = {1.2, 2.2, 3.2};
-        void *actual[3];
-        for(i = 0; i < noOfElements; i++)
-                actual[i] = &elementsToSort[i];
+    int i,noOfElements = 3;
+    float elementsToSort[] = {1.2, 3.2, 2.2}, expected[] = {1.2, 2.2, 3.2};
+    void *actual[3];
+    for(i = 0; i < noOfElements; i++)
+        actual[i] = &elementsToSort[i];
     bSort(actual,noOfElements,compareFloats);
-           ASSERT(expected[0] == *(float*)actual[0]);
-           ASSERT(expected[1] == *(float*)actual[1]);
-           ASSERT(expected[2] == *(float*)actual[2]);
-}
+    ASSERT(expected[0] == *(float*)actual[0]);
+    ASSERT(expected[1] == *(float*)actual[1]);
+    ASSERT(expected[2] == *(float*)actual[2]);
+};
 
 void test_sort_doubles_using_bSort(){
-        int i,noOfElements = 3;
-        double elementsToSort[] = {1.2, 3.2, 2.2}, expected[] = {1.2, 2.2, 3.2};
-        void *actual[3];
-        for(i = 0; i < noOfElements; i++)
-                actual[i] = &elementsToSort[i];
+    int i,noOfElements = 3;
+    double elementsToSort[] = {1.2, 3.2, 2.2}, expected[] = {1.2, 2.2, 3.2};
+    void *actual[3];
+    for(i = 0; i < noOfElements; i++)
+        actual[i] = &elementsToSort[i];
     bSort(actual,noOfElements,compareDoubles);
-           ASSERT(expected[0] == *(double*)actual[0]);
-           ASSERT(expected[1] == *(double*)actual[1]);
-           ASSERT(expected[2] == *(double*)actual[2]);
-}
+    ASSERT(expected[0] == *(double*)actual[0]);
+    ASSERT(expected[1] == *(double*)actual[1]);
+    ASSERT(expected[2] == *(double*)actual[2]);
+};
