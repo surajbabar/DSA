09a00331333694db9860da0e5368a12008e162d4
suraj <surajbab@thoughtworks.com>
2013-12-28 19:27:15 +0530

    code refactored

--- binary-search/bSearch.c
+++ binary-search/bSearch.c
@@ -3,13 +3,15 @@
 
 void* bSearch (void* key,void* base,int high,int size, CompareFPtr compare){
 	int low=0,mid,result;
+
 	qsort(base,high,size,compare);
+
 	while(low<high){
-		mid=(low+high)/2;
-		result = compare(key,base+mid*size);
-		if(0==result) return key;
-		if(result>0) low=mid+1;
-		else high=mid-1; 
+		mid = (low + high) / 2;
+		result = compare(key,base + mid * size);
+		if( 0 == result) return key;
+		if(result>0) low = mid + 1;
+		else high = mid - 1; 
 	}
 	return NULL;
 };
