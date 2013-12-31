a469e330121a0c52c91486b98ba5a34c77ffd7bc
suraj <surajbab@thoughtworks.com>
2013-12-31 15:15:03 +0530

    added ifndef

--- iterator/iterator.h
+++ iterator/iterator.h
@@ -1,6 +1,9 @@
+#ifndef Iterator_
+#define Iterator_
 typedef struct iterator{
 	void* list;
 	int position;
 	int (*hasNext)(struct iterator *it );
 	void* (*next)(struct iterator *it );
 }Iterator;
+#endif
